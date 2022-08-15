86aaef991adfef2992b1564efd43d9c2dac6d340
rock8181 <rock8181@naver.com>
2022-08-15 15:27:00 +0900

    initial commit

---
+++ project/target1.py
@@ -0,0 +1,81 @@
+#https://wikidocs.net/16107
+class Smartphone:
+    """
+    Smartphone Class
+    """
+    smartphone_count = 0
+
+    def __init__(self, brand, informations):
+        self._brand = brand
+        self._informations = informations
+        Smartphone.smartphone_count += 1
+
+    def __str__(self):
+        return f'str : {self._brand} - {self._informations}'
+
+    def __repr__(self):
+        return f'repr : {self._brand} - {self._informations}'
+
+    def detail_info(self):
+        print(f'Current Id : {id(self)}')
+        print(f"Smartphone Detail Info : {self._brand} {self._informations.get('price')}")
+        
+    def get_price(self):
+        return f"Before Smartphone Price -> brand : {self._brand}, price : {self._informations.get('price')}"
+
+    def get_price_culc(self):
+        pass
+        return f"After Smartphone Price -> brand : {self._brand}, price : {self._informations.get('price') * Smartphone.price_per_raise}"
+    
+    @classmethod
+    def raise_price(cls, per):
+        if per <= 1:
+            print('Please Enter 1 or More')
+            return
+        cls.price_per_raise = per
+        return 'Succeed! price increased.'
+
+    @staticmethod
+    def is_iphone(inst):
+        if inst._brand == 'Iphone':
+            return f'OK! This Smartphone is {inst._brand}.'
+        return 'Sorry. This Smartphone is not Iphone.'
+    
+    
+Smartphone1 = Smartphone('Iphone', {'color' : 'White', 'price': 10000})
+Smartphone2 = Smartphone('Galaxy', {'color' : 'Black', 'price': 8000})
+# 기본 정보
+print(Smartphone1)
+print(Smartphone2)
+
+Smartphone1._informations
+Smartphone2._informations
+
+print(Smartphone1.get_price())
+print(Smartphone2.get_price())
+
+# 가격 인상(클래스 메소드 미사용)
+# 이렇게 직접 접근은 좋지 않아요.
+print(Smartphone.price_per_raise2)
+Smartphone.price_per_raise = 1.2
+Smartphone.price_per_raise2 = 1.2
+
+# 가격 정보
+print(Smartphone1.get_price_culc())
+print(Smartphone2.get_price_culc())
+
+# 가격 인상
+Smartphone.raise_price(1.6)
+
+# 가격 정보
+print(Smartphone1.get_price_culc())
+print(Smartphone2.get_price_culc())
+
+def is_iphone(inst):
+    if inst._brand == 'Iphone':
+        return f'OK! This Smartphone is {inst._brand}.'
+    return 'Sorry. This Smartphone is not Iphone.'
+
+print(is_iphone(Smartphone2))
+
+print('Static : ', Smartphone.is_iphone(Smartphone1))
---
+++ test/test.py
@@ -0,0 +1,31 @@
+
+import unittest
+
+def custom_function(file_name):
+    with open(file_name, 'rt') as f:
+        return sum(1 for _ in f)
+# TestCase를 작성
+class CustomTests(unittest.TestCase): 
+
+    def test_runs(self):
+        """단순 실행여부 판별하는 테스트 메소드"""
+        with self.assertRaises(TypeError):
+            custom_function()
+            custom_function2()
+
+    def test_runs2(self):
+        """단순 실행여부 판별하는 테스트 메소드"""
+        with self.assertRaises(TypeError):
+            custom_function()
+            custom_function2()
+
+# unittest를 실행
+if __name__ == '__main__':  
+    unittest.main()
+
+
+#python unittest
+## 목적 : 단위 테스트로 작성 코드 검증
+## 방법
+### unittest.Testcase 상속하여 테스트 클래스 작성
+
